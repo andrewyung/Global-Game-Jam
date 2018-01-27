0eacc76ff0843ba99f68ab4a4025647a41aec1a9
andrewyung <andrewyung@uvic.ca>
2018-01-26 23:29:17 -0800

    Add Layout

---
+++ Global Game Jam/Assets/CameraOrientate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraOrientate : MonoBehaviour {
+
+    [SerializeField]
+    private Transform cameraTransform;
+
+    public static Transform mainCameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		if (mainCameraTransform == null)
+        {
+            if (cameraTransform != null)
+            {
+                mainCameraTransform = cameraTransform;
+            }
+            else
+            {
+                mainCameraTransform = Camera.main.transform;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.forward = -mainCameraTransform.transform.forward;
+    }
+}
---
+++ Global Game Jam/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float movementMultiplier = 250;
+
+    [SerializeField]
+    private float maxVelocityMagnitude = 25;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	public void moveDirection(Vector3 relativeDirection)
+    {
+        rb.velocity = Vector3.ClampMagnitude(relativeDirection * movementMultiplier, maxVelocityMagnitude);
+        Debug.Log(rb.velocity);
+    }
+}
